2ebc08568d6ef0af04070ab35d10b3cb1649c12a
Gnimuc <qiyupei@gmail.com>
2019-02-10 10:53:13 +0800

    Update generator.jl

--- gen/generator.jl
+++ gen/generator.jl
@@ -9,7 +9,6 @@ using Clang
 # define NK_INCLUDE_FONT_BAKING
 # define NK_INCLUDE_DEFAULT_FONT
 const NUKLEAR_H = joinpath(@__DIR__, "..", "deps", "usr", "include", "nuklear.h") |> normpath
-# const HELPER_H = joinpath(@__DIR__, "..", "deps", "usr", "include", "helper.h") |> normpath
 
 # create a work context
 ctx = DefaultContext()
@@ -62,3 +61,34 @@ end
 # uncomment the following code to generate dependency and template files
 # copydeps(dirname(api_file))
 # print_template(joinpath(dirname(api_file), "LibTemplate.jl"))
+
+const HELPER_H = joinpath(@__DIR__, "..", "deps", "usr", "include", "helper.h") |> normpath
+ctx = DefaultContext()
+parse_headers!(ctx, [HELPER_H], includes=[LLVM_INCLUDE])
+api_file = joinpath(@__DIR__, "helper_api.jl")
+api_stream = open(api_file, "w")
+for trans_unit in ctx.trans_units
+    root_cursor = getcursor(trans_unit)
+    push!(ctx.cursor_stack, root_cursor)
+    header = spelling(root_cursor)
+    @info "wrapping header: $header ..."
+    # loop over all of the child cursors and wrap them, if appropriate.
+    ctx.children = children(root_cursor)
+    for (i, child) in enumerate(ctx.children)
+        child_name = name(child)
+        child_header = filename(child)
+        ctx.children_index = i
+        # choose which cursor to wrap
+        startswith(child_name, "__") && continue  # skip compiler definitions
+        child_name in keys(ctx.common_buffer) && continue  # already wrapped
+        child_header != header && continue  # skip if cursor filename is not in the headers to be wrapped
+
+        wrap!(ctx, child)
+    end
+    @info "writing $(api_file)"
+    println(api_stream, "# Julia wrapper for header: $header")
+    println(api_stream, "# Automatically generated using Clang.jl\n")
+    print_buffer(api_stream, ctx.api_buffer)
+    empty!(ctx.api_buffer)  # clean up api_buffer for the next header
+end
+close(api_stream)
